a13a17bb34d8d7882c90f1959a27488ceca879ff
Yash Gupta <yashgupta50357@gmail.com>
2025-03-01 18:23:45 +0530

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Body } from "./components/Body";
+import { Login } from "./components/login";
+import { Profile } from "./components/Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
